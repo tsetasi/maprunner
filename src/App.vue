68c7a9d94e804675f0b4a07fb783924a1c2f77d6
Derrick Jensen <dsjensen@gmail.com>
2019-01-24 21:35:46 -0500

    Added preliminary item list and detail components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ItemList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ItemList from './components/ItemList';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ItemList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
 </style>
---
+++ src/components/AddItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form @submit="addItem" novalidate class="form-inline">
+            <md-field>
+                <label for="item name">Item name</label>
+                <md-input name="item name" id="item name" v-model="name" />
+            </md-field>
+            <md-card-actions>
+                <md-button type="submit" class="md-primary md-alignment-right">Create item</md-button>
+            </md-card-actions>
+        </form>
+    </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+
+export default {
+    name: "AddItem",
+    data() {
+        return {
+            name: ''
+        }
+    },
+    methods: {
+        addItem(e) {
+            e.preventDefault();
+            const newItem = {
+                id: uuid.v4(),
+                name: this.name,
+                count: null
+            }
+            this.$emit('add-item', newItem);
+            this.name = '';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
