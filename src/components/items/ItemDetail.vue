98eecb49c9a054c560388361589da01cdafd3e88
Derrick Jensen <dsjensen@gmail.com>
2019-02-24 16:16:30 -0500

    Items refactor, changing to incrementor buttons

--- src/components/items/ItemDetail.vue
+++ src/components/items/ItemDetail.vue
@@ -1,8 +1,17 @@
 <template>
-    <md-field>
-        <label>{{item.name}}</label>
-        <md-input v-model="item.count" min="0" type="number"></md-input>
-    </md-field>
+    <div class="md-layout md-gutter md-alignment-center-space-between">
+        <span class="md-layout-item">
+            <md-button class="incrementor md-icon-button md-raised md-primary" @click="item.count++">
+                <md-icon>add</md-icon>
+            </md-button>
+            <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="item.count <= 1" @click="item.count--">
+                <md-icon>remove</md-icon>
+            </md-button>
+        </span>
+        <span class="md-layout-item">
+            {{item.name}}: {{item.count}}
+        </span>
+    </div>
 </template>
 
 <script>
@@ -13,13 +22,5 @@ export default {
 </script>
 
 <style scoped>
-    input[type=number]::-webkit-outer-spin-button,
-    input[type=number]::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
-    input[type=number] {
-        -moz-appearance:textfield;
-    }
+    
 </style>
--- src/components/layout/RunDetail.vue
+++ src/components/layout/RunDetail.vue
@@ -2,30 +2,31 @@
     <md-dialog :md-active.sync="showDialog" :md-click-outside-to-close="false" :md-close-on-esc="false" :md-backdrop="true">
         <md-dialog-title>Portal Details</md-dialog-title>
         
-            <md-divider />
+        <md-divider />
 
-            <form novalidate>
-                <div class="md-layout md-gutter">
-                    <md-radio class="md-layout-item" v-model="portalType" value="canals">Canals</md-radio>
-                    <md-radio class="md-layout-item" v-model="portalType" value="altars">Altars</md-radio>
-                    <md-radio class="md-layout-item" v-model="portalType" value="hidden">Thief</md-radio>
+        <form novalidate>
+            <div class="md-layout md-gutter">
+                <md-radio class="md-layout-item" v-model="portalType" value="canals">Canals</md-radio>
+                <md-radio class="md-layout-item" v-model="portalType" value="altars">Altars</md-radio>
+                <md-radio class="md-layout-item" v-model="portalType" value="hidden">Thief</md-radio>
+            </div>
+
+            <div class="md-layout md-gutter md-alignment-center-center">
+                <h3 class="md-layout-item">
+                    {{portalType === 'altars' ? 'Invocation: ' : 'Floor: '}} {{floor}}
+                </h3>
+                <div class="md-layout-item">
+                    <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="isDisabled" @click="floor++">
+                        <md-icon>add</md-icon>
+                    </md-button>
+                    <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="floor <= 1" @click="floor--">
+                        <md-icon>remove</md-icon>
+                    </md-button>
                 </div>
+            </div>
+        </form>
 
-                <div class="md-layout md-gutter md-alignment-center-center">
-                    <h3 class="md-layout-item">
-                        {{portalType === 'altars' ? 'Invocation: ' : 'Floor: '}} {{floor}}
-                    </h3>
-                    <div class="md-layout-item">
-                        <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="!portalType" @click="floor++">
-                            <md-icon>add</md-icon>
-                        </md-button>
-                        <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="floor <= 1" @click="floor--">
-                            <md-icon>remove</md-icon>
-                        </md-button>
-                    </div>
-                </div>
-
-            </form>
+        <md-divider />
 
         <md-list>
             <md-list-item :key="index" v-for="(item, index) in items">
@@ -33,7 +34,7 @@
             </md-list-item>
         </md-list>
 
-            <md-divider />
+        <md-divider />
 
         <md-dialog-actions>
             <!-- <md-button class="md-primary" @click="showDialog = false">Cancel</md-button> -->
@@ -55,6 +56,11 @@ export default {
         portalType: 'canals',
         floor: 1
     }),
+    computed: {
+        isDisabled() {
+            return (!this.portalType || this.portalType === 'altars' ? (this.floor >= 5) : (this.floor >= 7));
+        }
+    },
     methods: {
         saveRun: function (e) {
             this.$emit('save-run', {'portalType': this.portalType, 'floor': this.floor})
