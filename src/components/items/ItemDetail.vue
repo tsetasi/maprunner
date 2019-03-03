7fa2f9f8ba5e07073a62e5a9c038e797b2122150
Derrick Jensen <dsjensen@gmail.com>
2019-03-03 17:32:15 -0500

    Swapping decrementor logic to update the correct thing

--- src/components/items/ItemDetail.vue
+++ src/components/items/ItemDetail.vue
@@ -4,7 +4,7 @@
             <md-button class="incrementor md-icon-button md-raised md-primary" @click="item.count++">
                 <md-icon>add</md-icon>
             </md-button>
-            <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="item.count <= 1" @click="item.count--">
+            <md-button class="incrementor md-icon-button md-raised md-primary" :disabled="item.count <= 0" @click="item.count--">
                 <md-icon>remove</md-icon>
             </md-button>
         </span>
